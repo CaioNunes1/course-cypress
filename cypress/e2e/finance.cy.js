cc8be82af3c41f5ffbc93dd307b4645f886767f2
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2025-03-21 18:10:21 -0300

    código testando cadastrar entrada

--- cypress/e2e/finance.cy.js
+++ cypress/e2e/finance.cy.js
@@ -1,5 +1,12 @@
 describe('Transações', () => {//describe servindo para falar quais funcion vamos testar
     it('Cadastrar uma entrada', () => {
         cy.visit("https://devfinance-agilizei.netlify.app/#")
+
+        cy.contains("Nova Transação").click()
+        cy.get('#description').type("Freela")
+        cy.get('#amount').type(250)
+        cy.get('#date').type("2025-03-21")//yyyy-mm-dd
+        cy.contains('button',"Salvar").click()
+    
     });
-});
+});;
