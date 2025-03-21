87b53ebf5ac4785f1835d974790daee8df6ede0f
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2025-03-21 17:46:24 -0300

    primeiro código

---
+++ cypress/e2e/finance.cy.js
@@ -0,0 +1,5 @@
+describe('Transações', () => {//describe servindo para falar quais funcion vamos testar
+    it('Cadastrar uma entrada', () => {
+        cy.visit("https://devfinance-agilizei.netlify.app/#")
+    });
+});
