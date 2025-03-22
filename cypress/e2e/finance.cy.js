63b2ecd41d02745c0e0d121fa8182e01f159e535
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2025-03-22 14:43:30 -0300

    Usando beforeEach

--- cypress/e2e/finance.cy.js
+++ cypress/e2e/finance.cy.js
@@ -1,19 +1,25 @@
 describe('Transações', () => {//describe servindo para falar quais funcion vamos testar
+
+    beforeEach(()=>{//antes de executar cada / todos os testes ele acessa a página da aplicação
+        cy.visit("https://devfinance-agilizei.netlify.app/#")
+    })
     it('Cadastrar uma entrada', () => {
         criarTransacao("Freela",250)
-        criarTransacao("Freela do fim de semana",100)
+        //criarTransacao("Freela do fim de semana",100)
 
         cy.get("tbody tr td.description").should('have.text', "Freela")
-        cy.get("tbody tr td.description").should('have.text', "Freela do fim de semana")
+        //cy.get("tbody tr td.description").should('have.text', "Freela do fim de semana")
     });
 
-    // it.skip('Cadastrar uma saída', () => {
+    it('Cadastrar uma saída', () => {
         
-    // });
+        criarTransacao("Cinema",-20)
+        cy.get("tbody tr td.description").should('have.text', "Cinema")
+    });
 });;
 
 function criarTransacao(descricao,valor){
-    cy.visit("https://devfinance-agilizei.netlify.app/#")
+    // cy.visit("https://devfinance-agilizei.netlify.app/#")
 
         cy.contains("Nova Transação").click()
         cy.get('#description').type(descricao)
