c7caf67de002d318067c71f1730b6bf4650c9982
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2025-03-22 11:46:42 -0300

    criando função em cypress

--- cypress/e2e/finance.cy.js
+++ cypress/e2e/finance.cy.js
@@ -1,12 +1,25 @@
 describe('Transações', () => {//describe servindo para falar quais funcion vamos testar
     it('Cadastrar uma entrada', () => {
-        cy.visit("https://devfinance-agilizei.netlify.app/#")
+        criarTransacao("Freela",250)
+        criarTransacao("Freela do fim de semana",100)
+
+        cy.get("tbody tr td.description").should('have.text', "Freela")
+        cy.get("tbody tr td.description").should('have.text', "Freela do fim de semana")
+    });
+
+    // it.skip('Cadastrar uma saída', () => {
+        
+    // });
+});;
+
+function criarTransacao(descricao,valor){
+    cy.visit("https://devfinance-agilizei.netlify.app/#")
 
         cy.contains("Nova Transação").click()
-        cy.get('#description').type("Freela")
-        cy.get('#amount').type(250)
+        cy.get('#description').type(descricao)
+        cy.get('#amount').type(valor)
         cy.get('#date').type("2025-03-21")//yyyy-mm-dd
         cy.contains('button',"Salvar").click()
     
-    });
-});;
+        
+}
